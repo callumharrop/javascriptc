bd2b6c802b4a0b61a2f38d5896aaba943ee4c1f1
codenation <codenation@codenations-MacBook-Pro.local>
2019-05-24 09:17:13 +0100

    Done the challange 1 for loops

---
+++ Arrays/ArrayChallanges.js
@@ -0,0 +1,20 @@
+//Challange 2//
+
+console.log(Array.from("GOOD BOY"))
+//Array.from() crates and array from a string or numbers//
+
+console.log(Array.from([1,2,3], x => x + x))
+
+//Challanges 3//
+
+let numbers = [
+    1,
+    2,
+    4,
+    8,
+]
+
+const map1 = numbers.map(x => x * 3)
+
+console.log(map1)
+
